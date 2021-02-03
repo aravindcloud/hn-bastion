89f02f028026a6531cbc61ff591d849c79ddc296
aravindcloud <69286075+aravindcloud@users.noreply.github.com>
2021-02-03 20:10:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,3 +59,47 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
+resource "azurerm_virtual_machine" "vm" {
+  count = var.vm_count
+  name = "vm-${count.index + 1}"
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size = var.vm_size
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = element(split("/", var.vm_image_string), 0)
+    offer = element(split("/", var.vm_image_string), 1)
+    sku = element(split("/", var.vm_image_string), 2)
+    version = element(split("/", var.vm_image_string), 3)
+  }
+
+  storage_os_disk {
+    name = "vm-${count.index + 1}-OS-Disk"
+    caching = "ReadWrite"
+    managed_disk_type = "Standard_LRS"
+    create_option = "FromImage"
+  }
+
+  # Optional data disks
+  storage_data_disk {
+    name = "vm-${count.index + 1}-Data-Disk"
+    disk_size_gb = "100"
+    managed_disk_type = "Standard_LRS"
+    create_option = "Empty"
+    lun = 0
+  }
+
+  os_profile {
+    computer_name = "healthnow${count.index + 1}"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+}
